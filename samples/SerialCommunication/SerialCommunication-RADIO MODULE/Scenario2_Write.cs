d2ce7ecb56b063bca38429581a8040da527cf168
José Simões <jose.simoes@eclo.solutions>
2021-09-06 12:37:33 +0100

    Add serial samples for ORGPAL

---
+++ samples/SerialCommunication/SerialCommunication-RADIO MODULE/Scenario2_Write.cs
@@ -0,0 +1,31 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.IO.Ports;
+
+namespace SerialCommunication
+{
+    public class Scenario2_Write
+    {
+        public static void Execute(ref SerialPort serialDevice)
+        {
+            // setup write timeout
+            // because we are writing to the UART it's recommended to set a write timeout
+            // otherwise the write operation doesn't return until the requested number of bytes has been written
+            serialDevice.WriteTimeout = 500;
+
+            for (; ; )
+            {
+                // write string followed by new line to Serial Device 
+                serialDevice.WriteLine(DateTime.UtcNow + " hello from nanoFramework MIOTY!");
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
