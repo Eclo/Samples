ac769d2bacaf8b0237d8aa17399cdd07bc296fb0
José Simões <jose.simoes@eclo.solutions>
2021-10-07 15:30:22 +0100

    Remove ESP32 code

--- samples/SerialCommunication/SerialCommunication-RADIO MODULE/Scenario1_ConfigureDevice.cs
+++ samples/SerialCommunication/SerialCommunication-RADIO MODULE/Scenario1_ConfigureDevice.cs
@@ -1,4 +1,4 @@
-//
+﻿//
 // Copyright (c) .NET Foundation and Contributors
 // See LICENSE file in the project root for full license information.
 //
@@ -7,20 +7,6 @@ using System;
 using System.Diagnostics;
 using System.Threading;
 using System.IO.Ports;
-#if BUIID_FOR_ESP32
-using nanoFramework.Hardware.Esp32;
-#endif
-
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-///                                                                        ///
-/// NOTE: when working with ESP32 edit the nfproj file and add             ///
-/// BUIID_FOR_ESP32 to the DefineConstants, like this:                     ///
-///                                                                        ///
-/// <DefineConstants>$(DefineConstants);BUIID_FOR_ESP32;</DefineConstants> ///
-///                                                                        ///
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
 
 namespace SerialCommunication
 {
@@ -39,21 +25,7 @@ namespace SerialCommunication
                 Debug.WriteLine($" {port}");
             }
 
-#if BUIID_FOR_ESP32
-            ////////////////////////////////////////////////////////////////////////////////////////////////////
-            // COM2 in ESP32-WROVER-KIT mapped to free GPIO pins
-            // mind to NOT USE pins shared with other devices, like serial flash and PSRAM
-            // also it's MANDATORY to set pin function to the appropriate COM before instantiating it
-
-            Configuration.SetPinFunction(32, DeviceFunction.COM2_RX);
-            Configuration.SetPinFunction(33, DeviceFunction.COM2_TX);
-
-            // open COM2
             _serialDevice = new SerialPort("COM2");
-#else
-            // open COM7 (connector 1)
-            _serialDevice = new SerialPort("COM2");
-#endif
             // set parameters
             _serialDevice.BaudRate = 9600;
             _serialDevice.Parity = Parity.None ;
@@ -66,13 +38,13 @@ namespace SerialCommunication
             // uncomment the scenario to test (!!note that none of these returns!!)
 
             // uncomment the following call to transmit data
-            //Scenario2_Write.Execute(ref _serialDevice);
+            Scenario2_Write.Execute(ref _serialDevice);
 
             // uncomment the following call to read data
             //Scenario3_Read.Execute(ref _serialDevice);
 
             // uncomment the following call to wait for a data received event
-            Scenario4_DataReceivedEvent.Execute(ref _serialDevice);
+            //Scenario4_DataReceivedEvent.Execute(ref _serialDevice);
 
             Thread.Sleep(Timeout.Infinite);
         }
